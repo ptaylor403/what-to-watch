c78952acd6fae6df986022b781776340360f8cf4
Trisha <ptaylor403@gmail.com>
2016-04-09 10:54:41 -0400

    ratings and average by id

--- movie_lib.py
+++ movie_lib.py
@@ -1,52 +1,84 @@
 import csv
 
 class Movie:
-    def __init__(self, row):
-        self.id = row[0]
-        self.title = row[1]
+    def __init__(self, movie_id, title):
+        self.id = movie_id
+        self.title = title
     #need id, movie name
-    #u.item
 
+    def __str__(self):
+        return '{}: {}'.format(self.id, self.title)
 
 class User:
     def __init__(self, row):
         self.user_id = row[0]
     #find all the user's ratings
-    #u.user
-
+    # find all movies the user has rated
+    def __str__(self):
+        return str(self.user)
 
 class Rating:
     def __init__(self, row):
         self.user_id = row[0]
         self.movie_id = row[1]
         self.rating = row[2]
-    # find all ratings for a movie, find how many ratings there are - add them together and divide the sum by the number of ratings
-    # find all ratings for each movie and associate the rating with the user
-    #calculate average rating for each movie
-    #u.info
+        # count the number of ratings per movie
+        # find all ratings for a movie, add them together and divide the sum by the number of ratings
+        # sort movies by rating average
 
+    def __str__(self):
+        return ('User: {}, Movie: {}, Rating: {}'.format(self.user_id, self.movie_id, self.rating))
 
-movie_list = []
+movies = {}
 with open('u.item', encoding='latin_1') as f:
-    reader = csv.reader(f, delimiter='|')
+    reader = csv.DictReader(f, fieldnames=['movie_id', 'title'], delimiter='|')
     for row in reader:
-        movie_list.append(Movie(row))
+        movie = Movie(row['movie_id'], row['title'])
+        movies[movie.id] = movie
 
-# print(len(movie_list))
+# movies_list = [movies[key] for key in movies]
+#
+# user_input = input('Enter part of a movie title: ').lower()
+#
+# for movie in movies_list:
+#     if user_input in movie.title.lower():
+#         print(movie)
 
-user_list = []
+# ID = input('Enter a movie ID: ')
+#
+# for movie in movies_list:
+#     print()
+
+users_list = []
 with open('u.user', encoding='latin_1') as f:
     reader = csv.reader(f, delimiter = '|')
     for row in reader:
-        user_list.append(User(row))
+        users_list.append(User(row))
 
-# print(len(user_list))
+# for user in users_list:
+#     print(user)
 
 rating_list = []
 with open('u.data', encoding='latin_1') as f:
-    reader = csv.reader(f, delimiter='\t')
+    reader = csv.reader(f, delimiter = '\t')
     for row in reader:
         rating_list.append(Rating(row))
 
-# print(len(rating_list))
-    ##??
+# for rating in rating_list:
+#     print(rating)
+
+
+def ratings_by_movie_id(ratings, movie_id):
+    movie_ratings = []
+    for rating in rating_list:
+        if movie_id == rating.movie_id:
+            movie_ratings.append(int(rating.rating))
+    count = len(movie_ratings)
+    average = sum(movie_ratings) / count
+    print('Number of user ratings: ', count)
+    print('Average rating: ', "%.2f" % average)
+    return movie_ratings
+
+movie_id = input('Movie ID: ')
+
+id_sorted = ratings_by_movie_id(rating_list, movie_id)
